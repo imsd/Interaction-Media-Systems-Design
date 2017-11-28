242e0dd69044eb52955e1832a12fcdab5431e6b1
Ming <ming@Mings-MacBook-Pro.local>
2017-11-28 15:09:25 -0500

    added project planet

---
+++ Project Planet/Assets/orbit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orbit : MonoBehaviour {
+	public GameObject planet1;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		orbitAround(); 
+	}
+	
+	// Update is called once per frame
+	void orbitAround () {
+		transform.RotateAround (planet1.transform.position, Vector3.right, speed * Time.deltaTime);
+	}
+}
